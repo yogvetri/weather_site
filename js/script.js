840e360d5b7528b056fccf94b802b0b0d1f41f1d
yogvetri <vetrivel23072003@gmail.com>
2024-03-03 12:41:03 +0530

    input field correcting

--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
-
-let apikey = ""
+let apikey = "d2d1ab6d25e27c5ee80c6cd5120d54bd";
 
 let getWeather = async (city) => {
-    let weatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=matric`;
+    let weatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
     let weatherObj = await fetch(weatherAPI);
-    let response = weatherObj.json();
+    let response = await weatherObj.json();
     return response;
 }
 
 async function CallWeather() {
     try {
-        let city = 'chennai'
-        let response = await getWeather(city);
-        console.log(response);
-        console.log("Country :",response.sys['country']);
-        console.log("City :", response.name)
-        console.log("Temparature :", response.main['temp']);
-        console.log("Minimum Temparature :", response.main['temp_min']);
-        console.log("Maximum Temparature :", response.main['temp_max']);
-        console.log("Description :",response.weather[0]['description'])
-        console.log("Wind Speed :",response.wind['speed'])
-            } catch (error) {
+        const group = document.querySelector('.group');
+        group.addEventListener('submit', async function (e) {
+            e.preventDefault();
+            let ci = document.getElementById('search');
+            let city = ci.value;
 
+            let response = await getWeather(city);
+            console.log(response);
+            console.log("Country :", response.sys['country']);
+            console.log("City :", response.name)
+            console.log("Temperature :", response.main['temp']);
+            console.log("Minimum Temperature :", response.main['temp_min']);
+            console.log("Maximum Temperature :", response.main['temp_max']);
+            console.log("Description :", response.weather[0]['description'])
+            console.log("Wind Speed :", response.wind['speed'])
+        });
+    } catch (error) {
         if (error['cod'] == 401) {
             let errorMessage = "Server can't be reached";
             console.log(errorMessage);
-        }else{
+        } else {
             console.log(error)
         }
     }
-    
-    // .then((response)=>{
-    //     console.log(response);
-    // })
- 
-    }
-    CallWeather();
+}
+
+CallWeather();
